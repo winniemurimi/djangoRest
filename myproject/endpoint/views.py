647d44e3a627ba60120a41cf180a2e2cc1095d41
Winnie <winniemurimi2@gmail.com>
2024-01-15 12:52:18 +0200

    SecondCommit

---
+++ myproject/endpoint/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+
+# Create your views here.
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from base.models import Item
+from .serializers import ItemSerializer
+
+
+
+@api_view(['GET'])
+def getData(request):
+    items = Item.objects.all()
+    serializer = ItemSerializer(items, many=True)
+    return Response(serializer.data)
+
