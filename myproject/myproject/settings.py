eac1932ad6ac607bc5f7f6f98cb29de76017523c
Winnie <winniemurimi2@gmail.com>
2024-02-20 11:42:19 +0200

    requirements

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-pinb$*$-^)v-)_my*w@bn*un)&h+kez&x^z0gbe-i1oi-q%^ls
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['15.236.52.50']
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
